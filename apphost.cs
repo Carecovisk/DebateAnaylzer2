57d645e850c311a5d4784f9f5b30061c7ba60efa
João Victor Ribeiro Santos <joaovictoribeirosantos11@gmail.com>
2026-07-31 22:33:58 -0300

    first second

--- apphost.cs
+++ apphost.cs
@@ -1,5 +1,14 @@
 #:sdk Aspire.AppHost.Sdk@13.4.6
+#:package Aspire.Hosting.JavaScript@13.4.6
+#:project src/DebateAnalyzer.Api/DebateAnalyzer.Api.csproj
 
 var builder = DistributedApplication.CreateBuilder(args);
+
+var api = builder.AddProject<Projects.DebateAnalyzer_Api>("api");
+
+var web = builder.AddViteApp("web", "src/DebateAnalyzer.Web", "start")
+    .WithReference(api)
+    .WaitFor(api)
+    .WithExternalHttpEndpoints();
 
 builder.Build().Run();
